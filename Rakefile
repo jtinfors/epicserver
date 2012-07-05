155ca2e82fb056d89340be694574622327d85908
Jens Tinfors <jens@tinfors.se>
2012-07-05 20:58:32 +0200

    Reverting back site to lobster state, they way it should be! :-)

--- Rakefile
+++ Rakefile
@@ -1,7 +1,9 @@
-# Add your own tasks in files placed in lib/tasks ending in .rake,
-# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
-require File.expand_path('../config/application', __FILE__)
 require 'rake'
+require 'rake/testtask'
 
-Epicserver::Application.load_tasks
+Rake::TestTask.new do |t|
+  t.libs << "test"
+  t.libs << "lib/rack"
+  t.test_files = FileList['test/epicserver_test.rb']
+  t.verbose = true
+end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Epicserver::Application.config.secret_token = '65cad154c6359b9880d6d13d6f229030d58528f8f0dddfac8249de2cd8e9808007ea5e739a411ede34df1f1f9383b1e88060aa308d85cd450bd0afa931ead1dc'
